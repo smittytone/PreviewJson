12b04aa7f26f07a24cd81d04906c00de50dfc3bc
smittytone <smittytone@icloud.com>
2024-04-26 23:45:10 +0100

    Refactor thumbnailer

--- JSON Thumbnailer/ThumbnailProvider.swift
+++ JSON Thumbnailer/ThumbnailProvider.swift
@@ -23,117 +23,90 @@ class ThumbnailProvider: QLThumbnailProvider {
         case badGfxBitmap
         case badGfxDraw
     }
-    
+
 
     override func provideThumbnail(for request: QLFileThumbnailRequest, _ handler: @escaping (QLThumbnailReply?, Error?) -> Void) {
 
         /*
          * This is the main entry point for macOS' thumbnailing system
          */
-        
-        let iconScale: CGFloat = request.scale
-        let thumbnailFrame: CGRect = NSMakeRect(0.0,
-                                                0.0,
-                                                CGFloat(BUFFOON_CONSTANTS.THUMBNAIL_SIZE.ASPECT) * request.maximumSize.height,
-                                                request.maximumSize.height)
-        
-        handler(QLThumbnailReply.init(contextSize: thumbnailFrame.size) { (context) -> Bool in
-            // Place all the remaining code within the closure passed to 'handler()'
-            
-            let result: Result<Bool, ThumbnailerError> = autoreleasepool { () -> Result<Bool, ThumbnailerError> in
-            
-                // Load the source file using a co-ordinator as we don't know what thread this function
-                // will be executed in when it's called by macOS' QuickLook code
-                if FileManager.default.isReadableFile(atPath: request.fileURL.path) {
-                    // Only proceed if the file is accessible from here
-                    do {
-                        // Get the file contents as a string, making sure it's not cached
-                        // as we're not going to read it again any time soon
-                        let data: Data = try Data.init(contentsOf: request.fileURL, options: [.uncached])
-                        
-                        // Get the string's encoding, or fail back to .utf8
-                        let encoding: String.Encoding = data.stringEncoding ?? .utf8
-                        
-                        // Check the string's encoding generates a valid string
-                        // NOTE This may not be necessary and so may be removed
-                        guard let _: String = String.init(data: data, encoding: encoding) else {
-                            return .failure(ThumbnailerError.badFileLoad(request.fileURL.path))
-                        }
 
-                        // Instantiate the common code within the closure
-                        let common: Common = Common.init(true)
-                        
-                        let jsonAtts: NSAttributedString = common.getAttributedString(data)
+        // Load the source file using a co-ordinator as we don't know what thread this function
+        // will be executed in when it's called by macOS' QuickLook code
+        if FileManager.default.isReadableFile(atPath: request.fileURL.path) {
+            // Only proceed if the file is accessible from here
+            do {
+                // Get the file contents as a string, making sure it's not cached
+                // as we're not going to read it again any time soon
+                let data: Data = try Data.init(contentsOf: request.fileURL, options: [.uncached])
 
-                        // Set the primary drawing frame and a base font size
-                        let jsonFrame: CGRect = NSMakeRect(CGFloat(BUFFOON_CONSTANTS.THUMBNAIL_SIZE.ORIGIN_X),
-                                                           CGFloat(BUFFOON_CONSTANTS.THUMBNAIL_SIZE.ORIGIN_Y),
-                                                           CGFloat(BUFFOON_CONSTANTS.THUMBNAIL_SIZE.WIDTH),
-                                                           CGFloat(BUFFOON_CONSTANTS.THUMBNAIL_SIZE.HEIGHT))
+                // Get the string's encoding, or fail back to .utf8
+                let encoding: String.Encoding = data.stringEncoding ?? .utf8
 
-                        // Instantiate an NSTextField to display the NSAttributedString render of the code
-                        let jsonTextField: NSTextField = NSTextField.init(labelWithAttributedString: jsonAtts)
-                        jsonTextField.frame = jsonFrame
+                // Check the string's encoding generates a valid string
+                // NOTE This may not be necessary and so may be removed
+                guard let _: String = String.init(data: data, encoding: encoding) else {
+                    handler(nil, ThumbnailerError.badFileLoad(request.fileURL.path))
+                    return
+                }
 
-                        // Generate the bitmap from the rendered code text view
-                        guard let bodyImageRep: NSBitmapImageRep = jsonTextField.bitmapImageRepForCachingDisplay(in: jsonFrame) else {
-                            return .failure(ThumbnailerError.badGfxBitmap)
-                        }
+                // Instantiate the common code within the closure
+                let common: Common = Common.init(true)
+                let jsonAtts: NSAttributedString = common.getAttributedString(data)
 
-                        // Draw the code view into the bitmap
-                        jsonTextField.cacheDisplay(in: jsonFrame, to: bodyImageRep)
-                        
-                        // Alternative drawing code to make use of a supplied context
-                        // NOTE 'context' passed in by the caller, ie. macOS QL server
-                        var drawResult: Bool = false
+                // Set the primary drawing frame and a base font size
+                let jsonFrame: CGRect = NSMakeRect(CGFloat(BUFFOON_CONSTANTS.THUMBNAIL_SIZE.ORIGIN_X),
+                                                   CGFloat(BUFFOON_CONSTANTS.THUMBNAIL_SIZE.ORIGIN_Y),
+                                                   CGFloat(BUFFOON_CONSTANTS.THUMBNAIL_SIZE.WIDTH),
+                                                   CGFloat(BUFFOON_CONSTANTS.THUMBNAIL_SIZE.HEIGHT))
+
+                // Instantiate an NSTextField to display the NSAttributedString render of the code
+                let jsonTextField: NSTextField = NSTextField.init(frame: jsonFrame)
+                jsonTextField.attributedStringValue = jsonAtts
+
+                // Generate the bitmap from the rendered code text view
+                guard let bodyImageRep: NSBitmapImageRep = jsonTextField.bitmapImageRepForCachingDisplay(in: jsonFrame) else {
+                    handler(nil, ThumbnailerError.badGfxBitmap)
+                    return
+                }
+
+                // Draw the code view into the bitmap
+                jsonTextField.cacheDisplay(in: jsonFrame, to: bodyImageRep)
+
+                if let image: CGImage = bodyImageRep.cgImage {
+                    // Just in case, make a copy of the cgImage, in case
+                    // `bodyImageReg` is freed
+                    if let cgImage: CGImage = image.copy() {
+                        // Calculate image scaling, frame size, etc.
+                        let thumbnailFrame: CGRect = NSMakeRect(0.0,
+                                                                0.0,
+                                                                CGFloat(BUFFOON_CONSTANTS.THUMBNAIL_SIZE.ASPECT) * request.maximumSize.height,
+                                                                request.maximumSize.height)
+
                         let scaleFrame: CGRect = NSMakeRect(0.0,
                                                             0.0,
-                                                            thumbnailFrame.width * iconScale,
-                                                            thumbnailFrame.height * iconScale)
-                        if let image: CGImage = bodyImageRep.cgImage {
-                            context.draw(image, in: scaleFrame, byTiling: false)
-                            drawResult = true
-                        }
+                                                            thumbnailFrame.width * request.scale,
+                                                            thumbnailFrame.height * request.scale)
 
-                        // Not sure why this is needed -- not using CA -- but it seems to help
-                        CATransaction.commit()
-
-                        if drawResult {
-                            return .success(true)
-                        } else {
-                            return .failure(ThumbnailerError.badGfxDraw)
-                        }
-                    } catch {
-                        // NOP: fall through to error
+                        // Pass a QLThumbnailReply and no error to the supplied handler
+                        handler(QLThumbnailReply.init(contextSize: thumbnailFrame.size) { (context) -> Bool in
+                            // `scaleFrame` and `cgImage` are immutable
+                            context.draw(cgImage, in: scaleFrame, byTiling: false)
+                            return true
+                        }, nil)
+                        return
                     }
                 }
 
-                // We didn't draw anything because of 'can't find file' error
-                return .failure(ThumbnailerError.badFileUnreadable(request.fileURL.path))
+                handler(nil, ThumbnailerError.badGfxDraw)
+                return
+            } catch {
+                // NOP: fall through to error
             }
+        }
 
-            // Pass the outcome up from out of the autorelease
-            // pool code to the handler as a bool, logging an error
-            // if appropriate
-            
-            switch result {
-                case .success(_):
-                    return true
-                case .failure(let error):
-                    switch error {
-                        case .badFileUnreadable(let filePath):
-                            NSLog("Could not access file \(filePath)")
-                        case .badFileLoad(let filePath):
-                            fallthrough
-                        case .badFileUnsupportedEncoding(let filePath):
-                            NSLog("Could not render file \(filePath)")
-                        default:
-                            NSLog("Could not render thumbnail")
-                    }
-            }
-
-            return false
-        }, nil)
+        // We didn't draw anything because of 'can't find file' error
+        handler(nil, ThumbnailerError.badFileUnreadable(request.fileURL.path))
     }
 
 }
